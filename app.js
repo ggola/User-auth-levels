23e13176b46f2b3634ae9fdb8bf1566beb3442c6
giuliogola <ggola80@gmail.com>
2019-09-04 13:49:29 +0200

    Add Hashing with bcrypt and Salting

--- app.js
+++ app.js
@@ -8,7 +8,9 @@ const bodyParser = require('body-Parser');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
 //const encrypt = require('mongoose-encryption');
-const md5 = require('md5');
+//const md5 = require('md5');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 const app = express();
 app.use(express.static("public"));
@@ -43,40 +45,51 @@ app.get("/register", function(req, res){
 });
 
 app.post("/register", function(req, res){
-  const newUser = new User({
-    email: req.body.username,
-    password: md5(req.body.password)
-  });
-  newUser.save(function(err){
-    if (err) {
-      console.log(err);
-    } else {
-      res.render('secrets');
-    }
+
+  bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+    const newUser = new User({
+      email: req.body.username,
+      //password: md5(req.body.password)
+      password: hash
+    });
+    newUser.save(function(err){
+      if (err) {
+        console.log(err);
+      } else {
+        res.render('secrets');
+      }
+    });
   });
 });
 
 app.post("/login", function(req, res){
   const username = req.body.username;
-  const password = md5(req.body.password);
+  //const password = md5(req.body.password);
+  const password = req.body.password;
   User.findOne({email: username}, function(err, foundUser){
     if (err) {
       console.log(err);
     } else {
       if (foundUser) {
-        if (foundUser.password === password) {
-          res.render('secrets');
-        } else {
-          console.log("Password is not correct");
-        }
+        // Compare user-entered password with foundUser hash (= .password) - saltRounds are stored during registration
+        bcrypt.compare(password, foundUser.password, function(err, result) {
+            if (result === true) {
+              res.render('secrets');
+            } else {
+              console.log("Password is not correct");
+            }
+        });
+        // if (foundUser.password === password) {
+        //   res.render('secrets');
+        // } else {
+        //   console.log("Password is not correct");
+        // }
       } else {
         console.log("User does not exist");
       }
     }
   });
 });
-
-
 
 app.listen(3000, function() {
   console.log("Server running on port 3000");
